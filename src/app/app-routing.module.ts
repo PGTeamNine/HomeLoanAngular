5ad6d212850078fb8bf05c4e9c219a64fbcccaf6
PGTeamNine <team9pg@gmail.com>
2022-07-21 08:24:24 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'homeLink',
+    component:HomeComponent
+  },
+  {
+    path:'registerLink',
+    component:RegisterComponent
+  },
+  {
+    path:'loginLink',
+    component:LoginComponent
+  },
+  {
+    path:'calculatorLink',
+    component:CalculatorComponent
+  },
+  // {
+  //   path:'dashboardLink',
+  //   component:DashboardComponent,
+  //   children:[
+  //     {
+  //       path:'courseLink', component:CourseComponent
+  //     },
+  //     {
+  //       path:'profileLink', component:ProfileComponent
+  //     },
+  //     {
+  //       path:'changepasswordLink', component:ChangepasswordComponent
+  //     }
+  //   ]
+  // },
+  {
+    path:'aboutLink',
+    component:AboutComponent
+  },
+  // {
+  //   path:'**',
+  //   component:
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
