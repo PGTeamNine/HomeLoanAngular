c36f877b63dd51ec48bc6cc86075480a576e75ca
PGTeamNine <team9pg@gmail.com>
2022-07-23 17:15:55 +0530

    Logo

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,37 +8,38 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-  x:any;
-  card:any ;
-  title = 'FirstApp';
-  formdata:any;
-  formdata1:any;
-  income:any;
-  roi:any;
-  loan:any;
-  roi1:any;
-  tenure:any;
-  loanEligibility:any;
-  loanEmi:any;
-  selected1= '';
-  selected2= '';
-  visible1='';
-  visible2='';
-  rotateAndChange:any;
+  title = 'Services';
+  x:any; // (1+r)^n
+  displayResult:any ; // display calculted output
+  formdata:any; // Data of ELIGIBILTY Form 
+  formdata1:any; // Data of EMI Form 
+  income:any; // Monthly income input of ELIGIBILTY Form
+  roi:any; // Rate of interest input of ELIGIBILTY Form
+  loan:any; // Loan amount input of EMI Form
+  roi1:any; // Rate of interest input of EMI Form
+  tenure:any; // Tenure input of EMI Form
+  loanEligibility:any; // Calculated Maximum eligible loan 
+  loanEmi:any; // Calculated EMI
+  selected1= ''; // When 1st(Eligibility) tab is selected this gets the value 'selected-tab'
+  selected2= ''; // When 2st(EMI) tab is selected this gets the value 'selected-tab'
+  visible1=''; // To make ELIGIBILTY FORM invisible wheck emi tab is clicked
+  visible2=''; // To make EMI FORM invisible wheck eligibilty tab is clicked
+
+  // On clicking ELIGIBILITY TAB
   public onClick1(){
     this.selected1 = 'selected-tab'
     this.selected2 = ''
     this.visible1=''
     this.visible2='calc-invisible'
   }
+  // On clicking EMI TAB
   public onClick2(){
     this.selected1 = ''
     this.selected2 = 'selected-tab'
     this.visible1='calc-invisible'
     this.visible2=''
   }
- 
- 
+  // On submiting ELIGIBILITY FORM
   public onSubmitEligibility(eliForm:any){
     this.income = eliForm.income;
     this.roi = eliForm.roi;
@@ -46,19 +47,16 @@ export class CalculatorComponent implements OnInit {
     this.loanEligibility = 60 * 0.6 *this.income;
     
     
-   this.card.innerHTML = `
+   this.displayResult.innerHTML = `
    <div>
       You are eligible for a loan amount of
        <span class="amount amount-highlight">${this.loanEligibility}</span>
     </div>
-    <a href="register.html"><button class="btn">apply now</button></a>
+    <a [routerLink]="['/registerLink']"><button class="btn">apply now</button></a>
    </div>
    `;
-    
-    
-    
-    
   }
+  // On submiting EMI FORM
   public onSubmitEmi(emiForm:any){
     this.loan = emiForm.loan;
     this.roi1 = (emiForm.roi1/12)/100;
@@ -70,11 +68,9 @@ export class CalculatorComponent implements OnInit {
     
     this.loanEmi = this.loan*this.roi1*(this.x/(this.x-1));
     // console.log(Math.round(this.loanEmi));
-this.loanEmi = Math.round(this.loanEmi);
+    this.loanEmi = Math.round(this.loanEmi);
     
-    
-    
-   this.card.innerHTML = `
+    this.displayResult.innerHTML = `
     <div>
       Monthly EMI
        <span class="amount amount-highlight">${this.loanEmi}</span>
@@ -91,15 +87,12 @@ this.loanEmi = Math.round(this.loanEmi);
        Loan payable
        <span class="amount">${this.loanEmi*this.tenure - this.loan}</span>
     </div>
-    <a href="register.html"><button class="btn">apply now</button></a>
-   `;
-    
-    
-    
-    
+    <a [routerLink]="['/registerLink']"><button class="btn">apply now</button></a>
+    `;
   }
+  // When this class gets initialized 
   ngOnInit():void{
-    this.card=  document.querySelector('.calc-description');
+    this.displayResult=  document.querySelector('.calc-description');
     this.selected1 = 'selected-tab'
     this.visible2='calc-invisible'
     this.formdata = new FormGroup({
@@ -113,7 +106,4 @@ this.loanEmi = Math.round(this.loanEmi);
       });
   }
   constructor() { }
-
-  
-
 }
